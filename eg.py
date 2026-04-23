963888a1d755d5c3a4b3437d8de8a2a86aacf69f
sumitshinde4585 <sumitshinde11114@gmail.com>
2026-04-23 19:27:05 +0530

    after stash pop file

--- eg.py
+++ eg.py
@@ -1,3 +1,7 @@
 # This is example file for git desktop
 
 print("The Git Desktop used here..")
+
+print("Here we use the stash ")
+#What is Git Stash?
+# Git stash temporarily saves your uncommitted changes so you can work on something else, then come back and re-apply them later
